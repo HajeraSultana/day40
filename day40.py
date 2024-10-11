eb6e837ec163ad5b4a0d42f36275507c2a12ed1c
HajeraSultana <e031306@gmail.com>
2024-10-11 07:59:33 +0100

    Create day40.py

---
+++ day40.py
@@ -0,0 +1,9 @@
+userDetails = { "name" : "Hajera Sultana", "dateOfBirth" : "13/10/1991", "contactNumber" : "07306280394", "email" : "e031306@gmil.com", "address" : "1 Canada Park Parade, London"}
+
+print("Here's your contact card")
+print()
+print(f"Name: {userDetails['name']}")
+print(f"DOB: {userDetails['dateOfBirth']}")
+print(f"Tel: {userDetails['contactNumber']}")
+print(f"Email: {userDetails['email']}")
+print(f"Address: {userDetails['address']}")
